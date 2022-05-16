6fe3c7b504b0db8656b8fabd35b73122f84fdb5b
npx <>
2022-05-16 19:22:18 +0200

    feat(wm): filter sticky windows from statusbar

--- wm/.config/sketchybar/plugins/windows.sh
+++ wm/.config/sketchybar/plugins/windows.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sketchybar -m --set windows label="$(yabai -m query --windows --space | jq -cr 'sort_by(.id) | map(if ."has-focus" then "[*\(.app)*]"  else "[ \(.app) ]" end) | join(" ")')"
+sketchybar -m --set windows label="$(yabai -m query --windows --space | jq -cr 'map(select(."is-sticky" == false)) | sort_by(.id) | map(if ."has-focus" then "[*\(.app)*]"  else "[ \(.app) ]" end) | join(" ")')"
