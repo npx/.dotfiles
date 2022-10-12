f8a5fae741dcd667f16ac2fc4d36f0fb57635268
npx <>
2022-10-12 12:22:13 +0200

    feat(nvim): setup surround plugin

--- nvim/.config/nvim/lua/npx/init.lua
+++ nvim/.config/nvim/lua/npx/init.lua
@@ -2,3 +2,7 @@ require("npx.lsp")
 require("npx.telescope")
 require("npx.treesitter")
 require("npx.mason")
+
+require("nvim-surround").setup({
+	-- Configuration here, or leave empty to use defaults
+})
