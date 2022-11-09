2b4b2f3a42a143a7e740b22a395c2d3977e6f859
npx <>
2022-11-09 22:29:30 +0100

    refactor(nvim): format lsp.lua

--- nvim/.config/nvim/lua/npx/lsp.lua
+++ nvim/.config/nvim/lua/npx/lsp.lua
@@ -1,171 +1,159 @@
 -- Setup nvim-cmp (Auto Complete)
-local cmp = require "cmp"
+local cmp = require("cmp")
 
-cmp.setup(
-    {
-        snippet = {
-            expand = function(args)
-                -- For `vsnip` user.
-                vim.fn["vsnip#anonymous"](args.body)
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			-- For `vsnip` user.
+			vim.fn["vsnip#anonymous"](args.body)
 
-                -- For `luasnip` user.
-                -- require('luasnip').lsp_expand(args.body)
+			-- For `luasnip` user.
+			-- require('luasnip').lsp_expand(args.body)
 
-                -- For `ultisnips` user.
-                -- vim.fn["UltiSnips#Anon"](args.body)
-            end
-        },
-        mapping = {
-            ["<C-n>"] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}),
-            ["<C-p>"] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}),
-            ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-f>"] = cmp.mapping.scroll_docs(4),
-            ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), {"i", "n"}),
-            ["<C-e>"] = cmp.mapping.close(),
-            ["<CR>"] = cmp.mapping.confirm(
-                {
-                    behavior = cmp.ConfirmBehavior.Insert,
-                    select = true
-                }
-            ),
-            ["<Tab>"] = cmp.mapping.confirm(
-                {
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true
-                }
-            )
-        },
-        sources = {
-            {name = "nvim_lsp"},
-            -- For vsnip user.
-            {name = "vsnip"},
-            -- For luasnip user.
-            -- { name = 'luasnip' },
+			-- For `ultisnips` user.
+			-- vim.fn["UltiSnips#Anon"](args.body)
+		end,
+	},
+	mapping = {
+		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "n" }),
+		["<C-e>"] = cmp.mapping.close(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Insert,
+			select = true,
+		}),
+		["<Tab>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+		-- For vsnip user.
+		{ name = "vsnip" },
+		-- For luasnip user.
+		-- { name = 'luasnip' },
 
-            -- For ultisnips user.
-            -- { name = 'ultisnips' },
+		-- For ultisnips user.
+		-- { name = 'ultisnips' },
 
-            {name = "buffer", keyword_length = 5}
-        }
-    }
-)
+		{ name = "buffer", keyword_length = 5 },
+	},
+})
 
 -- Configure Typescript LSP
-require "lspconfig".tsserver.setup {
-    capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-    -- Needed for inlayHints. Merge this table with your settings or copy
-    -- it from the source if you want to add your own init_options.
-    init_options = require("nvim-lsp-ts-utils").init_options,
-    --
-    on_attach = function(client, bufnr)
-        local ts_utils = require("nvim-lsp-ts-utils")
+require("lspconfig").tsserver.setup({
+	capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	-- Needed for inlayHints. Merge this table with your settings or copy
+	-- it from the source if you want to add your own init_options.
+	init_options = require("nvim-lsp-ts-utils").init_options,
+	--
+	on_attach = function(client, bufnr)
+		local ts_utils = require("nvim-lsp-ts-utils")
 
-        -- defaults
-        ts_utils.setup(
-            {
-                debug = false,
-                disable_commands = false,
-                enable_import_on_completion = false,
-                -- import all
-                import_all_timeout = 5000, -- ms
-                -- lower numbers = higher priority
-                import_all_priorities = {
-                    same_file = 1, -- add to existing import statement
-                    local_files = 2, -- git files or files with relative path markers
-                    buffer_content = 3, -- loaded buffer content
-                    buffers = 4 -- loaded buffer names
-                },
-                import_all_scan_buffers = 100,
-                import_all_select_source = false,
-                -- if false will avoid organizing imports
-                always_organize_imports = true,
-                -- filter diagnostics
-                filter_out_diagnostics_by_severity = {},
-                filter_out_diagnostics_by_code = {},
-                -- inlay hints
-                auto_inlay_hints = false,
-                inlay_hints_highlight = "Comment",
-                inlay_hints_priority = 200, -- priority of the hint extmarks
-                inlay_hints_throttle = 150, -- throttle the inlay hint request
-                inlay_hints_format = {
-                    -- format options for individual hint kind
-                    Type = {},
-                    Parameter = {},
-                    Enum = {}
-                    -- Example format customization for `Type` kind:
-                    -- Type = {
-                    --     highlight = "Comment",
-                    --     text = function(text)
-                    --         return "->" .. text:sub(2)
-                    --     end,
-                    -- },
-                },
-                -- update imports on file move
-                update_imports_on_move = false,
-                require_confirmation_on_move = false,
-                watch_dir = nil
-            }
-        )
+		-- defaults
+		ts_utils.setup({
+			debug = false,
+			disable_commands = false,
+			enable_import_on_completion = false,
+			-- import all
+			import_all_timeout = 5000, -- ms
+			-- lower numbers = higher priority
+			import_all_priorities = {
+				same_file = 1, -- add to existing import statement
+				local_files = 2, -- git files or files with relative path markers
+				buffer_content = 3, -- loaded buffer content
+				buffers = 4, -- loaded buffer names
+			},
+			import_all_scan_buffers = 100,
+			import_all_select_source = false,
+			-- if false will avoid organizing imports
+			always_organize_imports = true,
+			-- filter diagnostics
+			filter_out_diagnostics_by_severity = {},
+			filter_out_diagnostics_by_code = {},
+			-- inlay hints
+			auto_inlay_hints = false,
+			inlay_hints_highlight = "Comment",
+			inlay_hints_priority = 200, -- priority of the hint extmarks
+			inlay_hints_throttle = 150, -- throttle the inlay hint request
+			inlay_hints_format = {
+				-- format options for individual hint kind
+				Type = {},
+				Parameter = {},
+				Enum = {},
+				-- Example format customization for `Type` kind:
+				-- Type = {
+				--     highlight = "Comment",
+				--     text = function(text)
+				--         return "->" .. text:sub(2)
+				--     end,
+				-- },
+			},
+			-- update imports on file move
+			update_imports_on_move = false,
+			require_confirmation_on_move = false,
+			watch_dir = nil,
+		})
 
-        -- required to fix code action ranges and filter diagnostics
-        ts_utils.setup_client(client)
+		-- required to fix code action ranges and filter diagnostics
+		ts_utils.setup_client(client)
 
-        -- no default maps, so you may want to define some here
-        local opts = {silent = true}
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", ":TSLspOrganize<CR>", opts)
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", ":TSLspRenameFile<CR>", opts)
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", ":TSLspImportAll<CR>", opts)
-    end
-}
+		-- no default maps, so you may want to define some here
+		local opts = { silent = true }
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", ":TSLspOrganize<CR>", opts)
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", ":TSLspRenameFile<CR>", opts)
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", ":TSLspImportAll<CR>", opts)
+	end,
+})
 
-require "lspconfig".eslint.setup {
-    on_new_config = function(config, new_root_dir)
-        -- The "workspaceFolder" is a VSCode concept. It limits how far the
-        -- server will traverse the file system when locating the ESLint config
-        -- file (e.g., .eslintrc).
-        config.settings.workspaceFolder = {
-            uri = vim.loop.cwd(),
-            name = vim.fn.fnamemodify(new_root_dir, ":t")
-        }
-    end
-}
+require("lspconfig").eslint.setup({
+	on_new_config = function(config, new_root_dir)
+		-- The "workspaceFolder" is a VSCode concept. It limits how far the
+		-- server will traverse the file system when locating the ESLint config
+		-- file (e.g., .eslintrc).
+		config.settings.workspaceFolder = {
+			uri = vim.loop.cwd(),
+			name = vim.fn.fnamemodify(new_root_dir, ":t"),
+		}
+	end,
+})
 
-vim.diagnostic.config(
-    {
-        virtual_text = true
-    }
-)
+vim.diagnostic.config({
+	virtual_text = true,
+})
 
 -- Configure Omnisharp LSP
 local pid = vim.fn.getpid()
 local omnisharp_bin = "/Users/ybaron/omnisharp/run"
-local nvim_lsp = require "lspconfig"
+local nvim_lsp = require("lspconfig")
 
-require "lspconfig".omnisharp.setup {
-    cmd = {omnisharp_bin, "--languageserver", "--hostPID", tostring(pid)},
-    root_dir = nvim_lsp.util.root_pattern("*.csproj", "*.sln"),
-    capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-}
+require("lspconfig").omnisharp.setup({
+	cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
+	root_dir = nvim_lsp.util.root_pattern("*.csproj", "*.sln"),
+	capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+})
 
 -- Rust
-require "lspconfig".rust_analyzer.setup(
-    {
-        settings = {
-            ["rust-analyzer"] = {
-                assist = {
-                    importGranularity = "module",
-                    importPrefix = "self"
-                },
-                cargo = {
-                    loadOutDirsFromCheck = true
-                },
-                procMacro = {
-                    enable = true
-                }
-            }
-        }
-    }
-)
+require("lspconfig").rust_analyzer.setup({
+	settings = {
+		["rust-analyzer"] = {
+			assist = {
+				importGranularity = "module",
+				importPrefix = "self",
+			},
+			cargo = {
+				loadOutDirsFromCheck = true,
+			},
+			procMacro = {
+				enable = true,
+			},
+		},
+	},
+})
 
 -- Go
-require'lspconfig'.gopls.setup{}
+require("lspconfig").gopls.setup({})
