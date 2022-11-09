334de6e8c8503a79480ef157b128c6322bdd59b8
npx <>
2022-11-09 22:26:17 +0100

    feat(nvim): setup netrw

--- nvim/.config/nvim/lua/npx/init.lua
+++ nvim/.config/nvim/lua/npx/init.lua
@@ -6,3 +6,8 @@ require("npx.statusline")
 require("npx.surround")
 require("npx.telescope")
 require("npx.treesitter")
+
+-- Setup NetRW
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
