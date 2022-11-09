9a05b75fda9e196fccaf71cc563b8f556b0c45cd
npx <>
2022-11-09 20:39:17 +0100

    refactor(nvim): move surround plugin setup to file

--- nvim/.config/nvim/lua/npx/init.lua
+++ nvim/.config/nvim/lua/npx/init.lua
@@ -1,9 +1,6 @@
 require("npx.comment")
 require("npx.lsp")
+require("npx.surround")
 require("npx.telescope")
 require("npx.treesitter")
 require("npx.mason")
-
-require("nvim-surround").setup({
-	-- Configuration here, or leave empty to use defaults
-})
