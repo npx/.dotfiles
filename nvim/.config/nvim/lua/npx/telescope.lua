0db785f787752550faf32ef2047aadc854b91a40
npx <>
2022-11-16 22:00:45 +0100

    feat(nvim): search hidden files with find_files and live_grep

--- nvim/.config/nvim/lua/npx/telescope.lua
+++ nvim/.config/nvim/lua/npx/telescope.lua
@@ -5,28 +5,40 @@ local action_state = require("telescope.actions.state")
 local conf = require("telescope.config").values
 local actions = require("telescope.actions")
 
-require('telescope').setup{
-  defaults = {
-    file_ignore_patterns = { "%.mat", "%.meta", "%.asset", "%.prefab", "%.shader", "%.cginc", "%.asmdef", "%.unity" },
-    layout_strategy = "vertical"
-  },
-  mappings = {
-    i = {
-      ["<C-x>"] = false,
-      ["<C-q>"] = actions.send_to_qflist,
-    },
-  },
-}
+require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = {
+			".git/",
+			"%.mat",
+			"%.meta",
+			"%.asset",
+			"%.prefab",
+			"%.shader",
+			"%.cginc",
+			"%.asmdef",
+			"%.unity",
+		},
+		layout_strategy = "vertical",
+	},
+	mappings = {
+		i = {
+			["<C-x>"] = false,
+			["<C-q>"] = actions.send_to_qflist,
+		},
+	},
+})
 
 local M = {}
 
 M.project_files = function()
-  local opts = {
-	  -- git_command = { "git", "ls-files", "--exclude-standard" } 
-	  show_untracked = true
-  }
-  local ok = pcall(require"telescope.builtin".git_files, opts)
-  if not ok then require"telescope.builtin".find_files(opts) end
+	local opts = {
+		-- git_command = { "git", "ls-files", "--exclude-standard" }
+		show_untracked = true,
+	}
+	local ok = pcall(require("telescope.builtin").git_files, opts)
+	if not ok then
+		require("telescope.builtin").find_files(opts)
+	end
 end
 
 M.search_dotfiles = function()
@@ -47,4 +59,4 @@ M.git_branches = function()
 	})
 end
 
-return M;
+return M
--- nvim/.config/nvim/plugin/telescope.vim
+++ nvim/.config/nvim/plugin/telescope.vim
@@ -1,7 +1,7 @@
 nnoremap <silent><C-p> <cmd>lua require('npx.telescope').project_files()<CR>
 
-nnoremap <silent><leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
-nnoremap <silent><leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
+nnoremap <silent><leader>ff <cmd>lua require('telescope.builtin').find_files({ hidden = true })<cr>
+nnoremap <silent><leader>fg <cmd>lua require('telescope.builtin').live_grep({ additional_args = { "--hidden" } })<cr>
 nnoremap <silent><leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
 nnoremap <silent><leader>gc <cmd>lua require('npx.telescope').git_branches()<CR>
 
