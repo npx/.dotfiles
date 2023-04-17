f22b56ef4f40c64a3de3159e3fa4f760fe45a8d0
npx <>
2023-04-17 13:30:48 +0200

    feat(wm): nicer sketchybar based on nerd font

--- wm/.config/sketchybar/plugins/yabai-spaces.sh
+++ wm/.config/sketchybar/plugins/yabai-spaces.sh
@@ -2,27 +2,29 @@
 
 
 # icon mapping
-icon_default=display
-icon_company=building
-icon_comm=video
-icon_term=terminal
-icon_build=boxes-packing
-icon_vm=network-wired
-icon_private=user-secret
-
-sketchybar -m --default icon.padding_right=15
+icon_default=󰍹
+icon_company=
+icon_comm=󱩛
+icon_term=
+icon_build=
+icon_vm=
+icon_private=
 
 while read -r label
 do
   read -r index
   item=space_$label
   icon=icon_$label
-  sketchybar -m --add space $item center                          \
-                --set $item associated_display=1                  \
-                      associated_space=$index                     \
-                      icon.font="Font Awesome 6 Free:Solid:18.0"  \
-                      icon=${!icon:=•}                            \
-                      icon.highlight_color=0xf2cd2e4c             \
+  sketchybar -m --add space $item right                \
+                --set $item associated_display=1       \
+                      associated_space=$index          \
+                      icon=${!icon:=}                 \
+                      icon.highlight_color=0xf2cd2e4c  \
+                      label.padding_left=0             \
+                      label.padding_right=0            \
+                      icon.padding_left=0              \
+                      icon.padding_right=0             \
+                      background.padding_left=15       \
                       click_script="yabai -m space --focus $label || yabai -m space --focus recent"
 done < <(yabai -m query --spaces | jq -cr '.[] | (.label, .index)')
 
