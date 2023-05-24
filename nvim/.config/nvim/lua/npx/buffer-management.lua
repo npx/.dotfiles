df81c5abfe0c495103958f7709932abed0cd1a87
npx <>
2023-05-24 20:33:30 +0200

    feat(nvim): auto close buffers

--- nvim/.config/nvim/lua/npx/buffer-management.lua
+++ nvim/.config/nvim/lua/npx/buffer-management.lua
@@ -1,35 +1,111 @@
-local id = vim.api.nvim_create_augroup("startup", {clear = false})
+-- Const
+--
+local BUF_TRACKED = 'npx_tracked'
 
-local persistbuffer = function(bufnr)
-    bufnr = bufnr or vim.api.nvim_get_current_buf()
-    vim.fn.setbufvar(bufnr, 'bufpersist', 1)
+-- Util
+local cmd = function(cmd) return function() vim.cmd(cmd); end end
+
+local id = vim.api.nvim_create_augroup("npx_buffer_management", {clear = true})
+
+local autocmd = function(events, callback)
+    vim.api.nvim_create_autocmd(events, {
+        group = id,
+        pattern = {"*"},
+        callback = callback
+    })
 end
 
-vim.api.nvim_create_autocmd({"BufRead"}, {
-    group = id,
-    pattern = {"*"},
-    callback = function()
-        vim.api.nvim_create_autocmd({"InsertEnter", "BufModifiedSet"}, {
-            buffer = 0,
-            once = true,
-            callback = function() persistbuffer() end
-        })
-    end
-})
+-- Tracking Buffers
+local track = function(buf) vim.api.nvim_buf_set_var(buf, BUF_TRACKED, true) end
 
-vim.keymap.set('n', '<Leader>b', function()
-    local curbufnr = vim.api.nvim_get_current_buf()
-    local buflist = vim.api.nvim_list_bufs()
-    for _, bufnr in ipairs(buflist) do
-        if vim.bo[bufnr].buflisted and bufnr ~= curbufnr and
-            (vim.fn.getbufvar(bufnr, 'bufpersist') ~= 1) then
-            vim.cmd('bd ' .. tostring(bufnr))
+local untrack = function(buf)
+    local delete_tracked = function()
+        vim.api.nvim_buf_del_var(buf, BUF_TRACKED)
+    end
+    pcall(delete_tracked)
+end
+
+local is_tracked = function(buf)
+    local read_tracked = function()
+        vim.api.nvim_buf_get_var(buf, BUF_TRACKED)
+    end
+    return pcall(read_tracked)
+end
+
+-- Mark for kill
+local kill_buffer = nil
+
+local mark_for_kill = function(buf) kill_buffer = buf end
+
+local kill = function()
+    if kill_buffer == nil then return end
+
+    vim.api.nvim_buf_delete(kill_buffer, {})
+    kill_buffer = nil
+end
+
+local should_kill = function(buf)
+    local hasName = vim.api.nvim_buf_get_name(buf) ~= ""
+    local listed = vim.api.nvim_buf_get_option(buf, "buflisted")
+    local modified = vim.api.nvim_buf_get_option(buf, "modified")
+    local tracked = is_tracked(buf)
+
+    return listed and hasName and not modified and not tracked
+end
+
+-- Autocommands
+autocmd({"BufWritePost"}, function() track(0) end)
+
+autocmd({"BufHidden"}, function()
+    if should_kill(0) then
+        local buf = vim.api.nvim_get_current_buf()
+        mark_for_kill(buf)
+    end
+end)
+
+autocmd({"BufEnter"}, function() kill() end)
+
+-- Keymap: ALT+JK
+vim.keymap.set('n', '∆', cmd("bp"), {desc = 'Buffer Previous'})
+vim.keymap.set('n', '˚', cmd("bn"), {desc = 'Buffer Next'})
+
+local tracked_buffers = function()
+    local listed = vim.tbl_filter(function(buf)
+        return vim.api.nvim_buf_get_option(buf, "buflisted")
+    end, vim.api.nvim_list_bufs())
+
+    local decorated = vim.tbl_map(function(buf)
+        local bufname = vim.api.nvim_buf_get_name(buf)
+        if bufname == "" then bufname = "No Name" end
+        local name = vim.fn.fnamemodify(bufname, ":t")
+
+        local mod = vim.api.nvim_buf_get_option(buf, "modified") and "*" or " "
+        local display = name .. mod
+
+        local tracked = is_tracked(buf)
+        if tracked then
+            display = " " .. display .. " "
+        else
+            display = "(" .. display .. ")"
         end
-    end
-end, {silent = true, desc = 'Close unused buffers'})
 
--- " ALT+JKL;
-local keymap = vim.api.nvim_set_keymap;
+        local is_current = vim.api.nvim_get_current_buf() == buf
+        if is_current then
+            local hl_pre = "%#lualine_c_inactive#"
+            local hl_post = "%#lualine_c_normal#"
 
-keymap("n", "˙", ":bp<CR>", {noremap = true})
-keymap("n", "¬", ":bn<CR>", {noremap = true})
+            display = hl_pre .. display .. hl_post
+        end
+
+        return display
+    end, listed)
+
+    local pretty = table.concat(decorated, '')
+    return pretty
+end
+
+vim.keymap.set('n', 'Q', function() print(vim.inspect(tracked_buffers())) end)
+
+-- Module
+return {tracked_buffers = tracked_buffers}
+
--- nvim/.config/nvim/lua/npx/statusline.lua
+++ nvim/.config/nvim/lua/npx/statusline.lua
@@ -1,20 +1,22 @@
+local buffers = require("npx.buffer-management").tracked_buffers
+
 require("lualine").setup({
     options = {
         icons_enabled = true,
-        theme = 'auto',
-        component_separators = {left = '', right = ''},
-        section_separators = {left = '', right = ''},
+        theme = 'ayu_mirage',
+        component_separators = {left = '', right = ''},
+        section_separators = {left = '', right = ''},
         disabled_filetypes = {statusline = {}, winbar = {}},
         ignore_focus = {},
         always_divide_middle = true,
         globalstatus = false,
-        refresh = {statusline = 1000, tabline = 1000, winbar = 1000}
+        refresh = {statusline = 200, tabline = 1000, winbar = 1000}
     },
     sections = {
         lualine_a = {'mode'},
-        lualine_b = {'branch'},
-        lualine_c = {'buffers'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_b = {},
+        lualine_c = {buffers},
+        lualine_x = {'fileformat', 'filetype'},
         lualine_y = {},
         lualine_z = {'location'}
     },
