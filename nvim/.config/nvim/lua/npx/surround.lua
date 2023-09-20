58646bfd8da64c5437fe049fe8f44bb5c91d281f
npx <>
2023-09-20 19:21:44 +0200

    feat(nvim): surround bindings

--- nvim/.config/nvim/lua/npx/surround.lua
+++ nvim/.config/nvim/lua/npx/surround.lua
@@ -1,1 +1,6 @@
 require("nvim-surround").setup()
+
+vim.keymap.set("n", "<Leader>)", "ysiw)", {remap = true})
+vim.keymap.set("n", "<leader>]", "ysiw]", {remap = true})
+vim.keymap.set("n", "<leader>}", "ysiw}", {remap = true})
+
